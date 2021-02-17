274d92eb3e1d96a6c79d7b4fd9b8508b4f2e482f
udaysapare <uday2u.mca@gmail.com>
2021-02-17 23:15:11 +0530

    'Initial'

---
+++ 1datatypes.js
@@ -0,0 +1,138 @@
+String  = "Hi" 'bie' "434" '434.4' 'true' "false"
+Number = 8978 89787878 8.78 .877 898989.878877
+Boolean = true/false
+null
+undefined
+
+//es5////
+Declare Variable
+
+var a = 10
+var b = 'Hiii'
+var c = true
+
+typeof(a)
+"number"
+typeof(b)
+"string"
+typeof(c)
+"boolean"
+var d  = "10"
+
+typeof(d)
+"string"
+var e = 10.44
+
+typeof(e)
+"number"
+
+////////////////////////
+var a = 10
+var b = 20
+
+a+b
+30  (addition)
+a-b
+-10
+a*b
+200
+a/b
+0.5
+
+
+var a =4
+var b =2
+
+a%b
+0
+4%3
+1
+4%1
+0
+5%3
+2
+
+
+2%4
+2
+3%4
+3
+4%4
+0
+
+var a = "Hi"
+var b = "JavaScript"
+
+a+b
+"HiJavaScript" (concat)
+a-b
+NaN (not a number)
+a*b
+NaN
+a/b
+NaN
+a%b
+NaN
+
+
+var c = 10
+undefined
+a+b
+"HiJavaScript"
+a+c
+"Hi10"
+a-c
+NaN
+c+a
+"10Hi"
+a + ' '+b
+"Hi JavaScript"
+
+String+String=String
+String+Number= String
+Number+String=String
+Number+Number=Number
+
+10+"20"+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+"10"+20+30
+"1020"+30
+"102030"
+
+10+"20"+30-1
+"102030"-1    
+102029
+
+10+20+"30"-1
+"3030"-1
+3029
+
+"10"+20+30-1
+"102030"-1
+102029
+
+
+"a"-1
+NaN
+"1"-1
+0
+"10"+2
+"102"
+"10"-2
+8
+"10"-"2"
+8
+"10"*"2"
+20
+10*"2"
+20
+"1a"-1
+NaN
+"12"-1
+11
---
+++ udaytest.js
@@ -0,0 +1,5 @@
+////create new branch/////
+git checkout -b branchname
+git add .
+git commit -m "first commit"
+git push origin branchname
